87265c27edc33f446c38ea2e2a2fafd7558589d1
ANIMATION-15 <animation-15@ANIMATION-15s-iMac.local>
2026-01-07 11:23:46 +0700

    Latihan1

---
+++ Latihan1.py
@@ -0,0 +1,9 @@
+secret_word = "jeruk".upper()
+secret_word1 = list(secret_word)
+count = len(secret_word1)
+guess_letter = str(input("Guess letter : ")).upper()
+while True:
+    if count > 0:
+        
+    else:
+        print("You Win")
