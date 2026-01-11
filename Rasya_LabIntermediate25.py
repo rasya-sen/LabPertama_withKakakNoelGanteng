8dbb27cce5dee7657740cad7ef492d717776f086
Rasya <sendiputrarasya@gmail.com>
2026-01-11 20:13:14 +0700

    Rename dr Latihan1

---
+++ Rasya_LabIntermediate25.py
@@ -0,0 +1,47 @@
+lives = 6
+secret_word = "jeruk".upper()
+secret_word1 = list(secret_word)
+count = len(secret_word1)
+display_word = count * ["_"]
+while True:
+    if "_" in display_word and lives > 0:
+        validGuess = False
+        print("+--------------------------+")
+        print(" ")
+        print("Lives :", lives)
+        print("".join(display_word))
+
+        while validGuess == False:
+            guess = input("Guess a letter : ").upper()
+            print(" ")
+            countguess = len(guess)
+            if countguess > 1:
+                print("Please guess one letter at a time.")
+                validGuess = False
+            else: 
+                validGuess = True
+    
+        if guess in secret_word1:
+            print(guess, "is part of the word.")
+            for num in range(count):
+                if secret_word1[num] == guess:
+                    display_word[num] = guess
+        else:
+            print(guess, "is not part of the word.")
+            lives = lives - 1
+    elif lives <= 0:
+        print("+--------------------------+")
+        print(" ")
+        print("You Lose")
+        print("The word was,", secret_word)
+        print(" ")
+        print("+--------------------------+")
+        break
+    else:
+        print("+--------------------------+")
+        print(" ")
+        print("You Win!")
+        print("The word was,", secret_word)
+        print(" ")
+        print("+--------------------------+")
+        break
